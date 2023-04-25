0770bb70b6328166447e5ad8da6dea90eba7076c
Pavlo Yakubovskyi <pavloyakubovskyi@gmail.com>
2023-04-25 13:38:43 +0200

    add new repositories

---
+++ js/01-gallery.js
@@ -0,0 +1,6 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+// Створи галерею з можливістю кліку по її елементах і перегляду повнорозмірного зображення у модальному вікні.
+// Подивися демо відео роботи галереї.
