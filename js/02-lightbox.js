edc3c3d7697774388aa39e739f646591149715df
Pavlo Yakubovskyi <pavloyakubovskyi@gmail.com>
2023-04-29 23:23:58 +0200

    added hw07

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryUlEl = document.querySelector(".gallery");
+
+galleryUlEl.innerHTML = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+  })
+  .join("");
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  /* options */
+  captionsData: "alt",
+  captionDelay: 250,
+});
+lightbox.on("show.simplelightbox");
